9e8075c2177e0604efa9d8e46a38bc0225890990
Seyed M. Mirtaheri <s.m.mirtaheri@gmail.com>
2014-02-15 11:48:36 -0500

    some code rearrangement

--- js/cosmetics.js
+++ js/cosmetics.js
@@ -1,45 +1,30 @@
-$(window).resize( resizeWebSite);
-console.log ('navbar height is ' + $("#pageTitle").height());
-console.log ('tab bar height is ' + $("#tabs").height());
+$( "#tabs" ).tabs();
 
-function resizeWebSite(){
-
-	$("#panel").height($(window).height() - (140+  $("#navbar").height() + $("#pageTitle").height()));
-   	$("#map").height($(window).height() - (140+ $("#navbar").height() + $("#pageTitle").height()));
-        
-     console.log($("#map").height() / 2);   
-        
-     var loaderTop = ($("#map").height() - $("#loadingIndicator").height()) / 2 + $("#map").position().top;
-     var loaderLeft = (($("#map").width() - $("#loadingIndicator").width()) / 2) + $("#map").position().left;
-    
-        
-	$("#loadingIndicator").css({'position' : 'absolute' , 'left' : loaderLeft + 'px', 'top' : loaderTop + 'px'});    
-}
-
-resizeWebSite();
- 
 $('#initialClickMessage').html(T('Click on a region for details'));
 $('#panel').html(T('Loading ...'));
 $('#pageTitle').html(T('Minority Health Observatory'));
 $('#notesTitle').html(T('Methodological Notes'));
-$('#P1').html(T('Select a data layer from the menu below and click on map for detailed information.'));
-$('#P2').html(T('Click on ') + '<img src="images/Information.jpg" width="25" height="25" title="InformationImage"/>' + T('for data sources and methodology.'));
 
+$('#HOME').html(T('HOME'));
+$('#INTERACTIVEMAPS').html(T('INTERACTIVE MAPS'));
+$('#ANALYSIS').html(T('ANALYSIS'));
+$('#PARTNERS').html(T('PARTNERS'));
 
 $('#support').html(T('With the Support of:'));
-$('#INTERACTIVE MAPS').html(T('INTERACTIVE MAPS'));
 
+
+$('#financialSupport').html(T('Financial Support:'));
 
 $('#MINISTRYOF').html(T('MINISTRY OF '));
-
+$('#LONGTERMCARE').html(T('HEALTH AND LONG-TERM CARE'));
 
 $('#dataProviders').html(T('Data Providers:'));
 $('#Health').html(T('Health'));
 $('#Force').html(T('Force'));
-
-
-
-
+$('#MINP1').html(T('MINISTRY OF '));
+$('#MINP2').html(T('MINISTRY OF '));
+$('#LONGTERMP1').html(T('HEALTH AND LONG-TERM CARE'));
+$('#LONGTERMP2').html(T('HEALTH AND LONG-TERM CARE'));
 
 
 if (location.hash != '#french')
@@ -57,7 +42,7 @@ else
 
 
 function T(str)
- {
+{
 	if (location.hash != '#french')
 		return str;
 	

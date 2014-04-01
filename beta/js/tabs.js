601fb37ba5f6be337c300f5c29a816315f750962
Ava-A <ava.ahadipour@gmail.com>
2014-03-31 20:12:04 -0400

    Issue #79

--- beta/js/tabs.js
+++ beta/js/tabs.js
@@ -50,7 +50,7 @@ Tabs.prototype.getMaps = function ()
 			'<div id="map" class="col-xs-12" data-dojo-type="dijit.layout.ContentPane" region="center"></div>' +
 			'<img src="images/loading2.gif" id="loadingIndicator"/>' +
 			
-			'<div class="mapsInstruction alert alert-success smallFont cursorStyle" id="instruction">' +
+			'<div class="mapsInstruction alert alert-success smallInstruction cursorStyle" id="instruction">' +
 				'<object data="images/information.svg" type="image/svg+xml" id="object"></object>'+
 				'<p id="mapsInstructionOne" class="mapsInstructionOne cursorStyle" align="center"></p>' +
 			'</div>' +

59f62accd1706493d568df11f0c5313ae733e172
Ava-A <ava.ahadipour@gmail.com>
2014-04-20 12:32:40 -0400

    -

--- beta/js/router.js
+++ beta/js/router.js
@@ -29,27 +29,24 @@ Router.prototype.route = function()
 	{
 		case 'home':
 			var body = tabs.getHome();
-			$('#homeTab').removeClass('selectedTab');
 			break;
 	
 		case 'maps':
 			var body = tabs.getMaps();
-			$('#homeTab').removeClass('selectedTab');
 			break;
 	
 		case 'analysis':
 			var body = tabs.getAnalysis();
-			$('#homeTab').removeClass('selectedTab');
 			break;
 	
 		case 'partners':
 			var body = tabs.getPartners();
-			$('#homeTab').removeClass('selectedTab');
 			break;
 			
 		default:
 			var body = tabs.getHome();
-			$('#homeTab').addClass('selectedTab');
+
+		$('#homeTab').removeClass('selectedTab');
 	
 	}
 	

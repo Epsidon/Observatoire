e93cfe4721b85c790051c24430f98e48e16124f0
Seyed M. Mirtaheri <s.m.mirtaheri@gmail.com>
2014-04-23 12:11:43 -0400

    removing hold from hospital legend list

--- beta/js/maps.js
+++ beta/js/maps.js
@@ -315,7 +315,7 @@ function(
 			}
 
 			map.addLayer(mapLayer[clickedServicePoint]);
-			mapServicePointLegendLabel += " - " +legendLabel[clickedServicePoint];
+			mapServicePointLegendLabel += " - " + legendLabel[clickedServicePoint];
 			numServicePointLayers++;				
 			map.reorderLayer(mapLayer[clickedServicePoint], numServicePointLayers);
 		}
